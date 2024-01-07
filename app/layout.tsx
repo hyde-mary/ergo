610e3ee8c667a8ce39093f2c2d392cdff842e5e8
Hydemary <hydemaryalzbayne@gmail.com>
2024-01-07 20:44:32 +0800

    feat: wrapped ThemeProvider with ConvexClientProvider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
+import { Toaster } from "sonner";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/providers/theme-provider";
+import { ConvexClientProvider } from "@/components/providers/convex-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,16 +20,19 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider
-        attribute="class"
-        defaultTheme="system"
-        enableSystem
-        disableTransitionOnChange
-        storageKey="ergo-theme-1"
-        >
-          {children}
-        </ThemeProvider>
-        </body>
+        <ConvexClientProvider>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+            storageKey="ergo-theme-1"
+          >
+          <Toaster position="bottom-center" />
+            {children}
+          </ThemeProvider>
+        </ConvexClientProvider>
+      </body>
     </html>
   );
 }
