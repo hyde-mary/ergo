a6e61bc519114dd0810a1a443c278c18ab237345
Hydemary <hydemaryalzbayne@gmail.com>
2024-01-07 20:47:16 +0800

    feat: added login and signout buttons in navbar aswell as connecting to database

--- app/(landing)/_components/navbar.tsx
+++ app/(landing)/_components/navbar.tsx
@@ -5,8 +5,13 @@ import { cn } from "@/lib/utils";
 import { Logo } from "./logo";
 import { Button } from "@/components/ui/button";
 import { ModeToggle } from "@/components/mode-toggle";
+import { useConvexAuth } from "convex/react";
+import { SignInButton, UserButton } from "@clerk/clerk-react";
+import { Spinner } from "@/components/spinner";
+import Link from "next/link";
 
 export const Navbar = () => {
+    const { isAuthenticated, isLoading } = useConvexAuth();
     const scrolled = useScrollTop();
     
     return(
@@ -16,6 +21,37 @@ export const Navbar = () => {
         )}>
             <Logo />
             <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
+                {isLoading && (
+                    <p className="mx-4"><Spinner /></p>
+                )}
+                {!isAuthenticated && !isLoading && (
+                    <>
+                        <SignInButton mode="modal">
+                            <Button variant="ghost" size="sm" className="border-b dark:border dark:shadow-md mx-1">
+                                Login
+                            </Button>
+                        </SignInButton>
+                        <SignInButton mode="modal">
+                            <Button size="sm" className="border-b dark:border dark:shadow-md mx-1">
+                                Get Ergo
+                            </Button>
+                        </SignInButton>
+                    </>
+                )}
+                {isAuthenticated && !isLoading && (
+                    <>
+                    <Button className="border-b dark:border dark:shadow-md" variant="ghost" size="sm" asChild>
+                        <Link href="/documents">
+                            Go to Documents
+                        </Link>
+                    </Button>
+                    <div className="mx-2">
+                    <UserButton 
+                        afterSignOutUrl="/"
+                    />
+                    </div>
+                    </>
+                )}
                 <ModeToggle/>
             </div>
         </div>
