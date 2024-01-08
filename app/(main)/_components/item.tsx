f9955c99f59969da12b7bf2b2438483f9add2a2b
Hydemary <hydemaryalzbayne@gmail.com>
2024-01-08 19:20:07 +0800

    feat: added onCreate and onArchive

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -1,8 +1,16 @@
 "use client";
 
+import { Skeleton } from "@/components/ui/skeleton";
+import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
 import { cn } from "@/lib/utils";
-import { ChevronDown, ChevronRight, LucideIcon } from "lucide-react";
+import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator } from "@/components/ui/dropdown-menu";
+import { useMutation } from "convex/react";
+import { ChevronDown, ChevronRight, LucideIcon, MoreHorizontal, PlusCircle, Trash2 } from "lucide-react";
+import { useRouter } from "next/navigation";
+import React from "react";
+import { toast } from "sonner";
+import { useUser } from "@clerk/clerk-react";
 
 interface ItemProps{
     id?: Id<"documents">;
@@ -13,7 +21,7 @@ interface ItemProps{
     level?: number;
     onExpand?: () => void;
     label: string;
-    onClick: () => void;
+    onClick?: () => void;
     icon: LucideIcon;
 };
 
@@ -30,7 +38,53 @@ export const Item = ({
     expanded,
 }: ItemProps) => {
 
+    const { user } = useUser();
+    const create = useMutation(api.documents.create);
+    const archive = useMutation(api.documents.archive);
+    const router = useRouter();
+
+    const onArchive = (
+        event: React.MouseEvent<HTMLDivElement, MouseEvent>
+    ) => {
+        event.stopPropagation();
+        if (!id) return;
+        const promise = archive({ id });
+
+        toast.promise(promise, {
+            loading: "Deleting...",
+            success: "Moved to trash!",
+            error: "Failed to archive :("
+        })
+    }
+
+    const handleExpand = (
+        event: React.MouseEvent<HTMLDivElement, MouseEvent>
+    ) => {
+        event.stopPropagation();
+        onExpand?.();
+    }
+
     const ChevronIcon = expanded ? ChevronDown : ChevronRight
+
+    const onCreate = (
+        event: React.MouseEvent<HTMLDivElement, MouseEvent>
+    ) => {
+        event.stopPropagation();
+        if (!id) return;
+        const promise = create({title: "Untitled", parentDocument: id})
+        .then((documentId)=>{
+            if (!expanded){
+                onExpand?.();
+            }
+            //router.push(`/documents/${documentId}`);
+        });
+
+        toast.promise(promise, {
+            loading: "Creating a new task...",
+            success: "New note task!",
+            error: "Failed to create new task :("
+        });
+    };
 
     return(
         <div
@@ -48,7 +102,7 @@ export const Item = ({
                     <div
                     role="button"
                     className="h-full rounded-sm hover:bg-neutral-300 dark:bg-neutral-600 mr-1"
-                    onClick={()=>{}}
+                    onClick={handleExpand}
                     >
                         <ChevronIcon
                         className="h-4 w-4 shrink-0 text-muted-foreground/50"
@@ -64,13 +118,60 @@ export const Item = ({
                     <Icon className="shrink-0 h-19 mr-2 text-muted-foreground"/>
                 )}
             <span className="truncate">
-            {label}
+                {label}
             </span>
             {isSearch && (
                 <kbd className="ml-auto pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
-                    <span className="text-xs">⌘</span>K
+                    <span className="text-xs">ctrl/⌘ </span>K
                 </kbd>
             )}
+            {!!id && (
+                <div className="ml-auto flex items-center gap-x-2">
+                    <DropdownMenu>
+                        <DropdownMenuTrigger onClick={(e) => e.stopPropagation()} asChild>
+                            <div 
+                            role="button"
+                            className="opacity-0 group-hover:opacity-100 h-full ml-auto rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600">
+                                <MoreHorizontal className="h-5 w-5 text-muted-foreground"/>
+                            </div>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent
+                        className="w-60"
+                        align="start"
+                        side="right"
+                        forceMount
+                        >
+                            <DropdownMenuItem onClick={onArchive}>
+                                <Trash2 className="h-4 w-4 mr-2"/>
+                                Delete
+                            </DropdownMenuItem>
+                            <DropdownMenuSeparator />
+                                <div className="text-xs text-muted-foreground p-2">
+                                    Last edited by: {user?.fullName} 
+                                </div>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+                    <div 
+                    role="button"
+                    onClick={onCreate}
+                    className="opacity-0 group-hover:opacity-100 h-full ml-auto rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600">
+                        <PlusCircle className="h-4 w-4 text-muted-foreground"/>
+                    </div>
+                </div>
+            )}
+        </div>
+    )
+}
+
+Item.Skeleton = function ItemSkeleton({ level }: { level?: number}){
+    return (
+        <div style={{
+                paddingLeft: level ? `${(level * 12) + 25}px` : "12px"
+            }}
+            className="flex gap-x-2 py-[3px]"
+        >
+            <Skeleton className="h-4 w-4"/>
+            <Skeleton className="h-4 w-[30%]"/>
         </div>
     )
 }
