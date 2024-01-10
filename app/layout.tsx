883604d32ea9d26166b16382d12f625f88e732e1
Hydemary <hydemaryalzbayne@gmail.com>
2024-01-10 20:54:59 +0800

    refactor: code changes and bug fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { ConvexClientProvider } from "@/components/providers/convex-provider";
+import { ModalProvider } from "@/components/providers/model-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -29,6 +30,7 @@ export default function RootLayout({
             storageKey="ergo-theme-1"
           >
           <Toaster position="bottom-center" />
+          <ModalProvider />
             {children}
           </ThemeProvider>
         </ConvexClientProvider>
