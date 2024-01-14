50e5baf5af1e75999ddc82735ac63237a3c44350
Hydemary <hydemaryalzbayne@gmail.com>
2024-01-14 13:36:26 +0800

    feat: publish functionality

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -116,7 +116,7 @@ export const Item = ({
                         {documentIcon}
                     </div>
                 ):(
-                    <Icon className="shrink-0 h-19 mr-2 text-muted-foreground"/>
+                    <Icon className="shrink-0 h-[18px] w-[18px] mr-2 text-muted-foreground"/>
                 )}
             <span className="truncate">
                 {label}
