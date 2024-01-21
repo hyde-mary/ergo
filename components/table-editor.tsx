b299563a6d62be69ecafe07df0d2b33ab8c38393
Hydemary <hydemaryalzbayne@gmail.com>
2024-01-21 19:33:08 +0800

    refactor: changed create task dialog

--- components/table-editor.tsx
+++ components/table-editor.tsx
@@ -16,14 +16,22 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import {
   CalendarCheck,
+  CalendarIcon,
   Clock,
-  Flag,
   Link,
   Mail,
   Pencil,
   PersonStanding,
 } from "lucide-react";
-import { Calendar } from "@/components/ui/calendar";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { Calendar } from "./ui/calendar";
+import { FormControl } from "./ui/form";
+import * as z from "zod";
+import { Textarea } from "./ui/textarea";
 
 async function getData(): Promise<Payment[]> {
   // Fetch data from your API here.
@@ -40,6 +48,16 @@ async function getData(): Promise<Payment[]> {
     },
   ];
 }
+
+const formSchema = z.object({
+  title: z.string().min(2).max(50),
+  due: z.date().min(new Date("2023-01-01")),
+  assigned: z.string().min(2).max(50),
+  delegator: z.string().min(2).max(50),
+  reminder: z.date().min(new Date("2023-01-01")),
+  subject: z.string().min(2).max(50),
+  e_body: z.string(),
+});
 
 const TableEditor = () => {
   const [data, setData] = useState<Payment[]>([]);
@@ -62,6 +80,7 @@ const TableEditor = () => {
               Create Tasks
             </Button>
           </DialogTrigger>
+
           <DialogContent className="sm:max-w-md">
             <DialogHeader>
               <DialogTitle>Create Tasks</DialogTitle>
@@ -69,73 +88,117 @@ const TableEditor = () => {
                 Edit the form below to create your task
               </DialogDescription>
             </DialogHeader>
-            <div className="flex items-center space-x-2">
+
+            <div className="flex items-center space-x-2 justify-end">
               <div className="grid flex-1 gap-2">
                 <div className="flex items-center">
-                  <Flag className="mr-2" />
-                  <Label htmlFor="event_name" className="text-left">
-                    Event Name:
+                  <PersonStanding className="mr-2" />
+                  <Label htmlFor="task" className="text-left">
+                    Title:
                   </Label>
+
+                  <Input className="ml-4" type="task-title" placeholder="Task Title" />
                 </div>
-                <div className="flex items-center">
+
+                <div className="flex items-center justify-start">
                   <CalendarCheck className="mr-2" />
                   <Label htmlFor="event_name" className="text-left">
-                    Due Date:
+                    Due:
                   </Label>
-                  <Calendar
-                    mode="single"
-                    disabled={(date) =>
-                      date > new Date() || date < new Date("1900-01-01")
-                    }
-                    initialFocus
-                  />
+
+                  <div className="ml-4">
+                    <Popover>
+                      <PopoverTrigger asChild>
+                        <Button
+                          variant={"outline"}
+                          className="w-[320px] pl-3 text-left font-normal"
+                        >
+                          Pick a date
+                          <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                        </Button>
+                      </PopoverTrigger>
+                      <PopoverContent className="w-auto p-0" align="start">
+                        <Calendar
+                          mode="single"
+                          onSelect={() => {}}
+                          disabled={(date) =>
+                            date > new Date() || date < new Date("1900-01-01")
+                          }
+                          initialFocus
+                        />
+                      </PopoverContent>
+                    </Popover>
+                  </div>
                 </div>
+
                 <div className="flex items-center">
                   <PersonStanding className="mr-2" />
                   <Label htmlFor="event_name" className="text-left">
-                    Assigned By:
+                    Assigned:
                   </Label>
+
+                  <Input className="ml-4" type="assigned_by" placeholder="Delegator Name" />
                 </div>
-                <Input type="assigned_by" placeholder="Delegator Name" />
-                <div className="flex items-center">
-                  <PersonStanding className="mr-2" />
-                  <Label htmlFor="event_name" className="text-left">
-                    Assigned Personnel:
-                  </Label>
-                </div>
-                <Input
-                  type="assigned_personnel"
-                  placeholder="Assigned Personnel"
-                />
+
                 <div className="flex items-center">
                   <Link className="mr-2" />
                   <Label htmlFor="event_name" className="text-left">
-                    Complementary Links:
+                    Links:
                   </Label>
+                  <Input className="ml-4 w-full" type="links" placeholder="Complementary Links" />
                 </div>
-                <Input type="links" placeholder="Complementary Links" />
+
                 <div className="flex items-center">
                   <Clock className="mr-2" />
                   <Label htmlFor="event_name" className="text-left">
-                    Send Reminder by:
+                    Reminder:
                   </Label>
+
+                  <div className="ml-4">
+                    <Popover>
+                      <PopoverTrigger asChild>
+                        <Button
+                          variant={"outline"}
+                          className="w-[280px] pl-3 text-left font-normal"
+                        >
+                          Pick a date
+                          <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                        </Button>
+                      </PopoverTrigger>
+                      <PopoverContent className="w-auto p-0" align="start">
+                        <Calendar
+                          mode="single"
+                          onSelect={() => {}}
+                          disabled={(date) =>
+                            date > new Date() || date < new Date("1900-01-01")
+                          }
+                          initialFocus
+                        />
+                      </PopoverContent>
+                    </Popover>
+                  </div>
                 </div>
+
                 <div className="flex items-center">
                   <Pencil className="mr-2" />
                   <Label htmlFor="event_name" className="text-left">
                     Subject:
                   </Label>
+
+                  <Input className="ml-4" type="subject" placeholder="Subject" />
                 </div>
-                <Input type="subject" placeholder="Subject" />
+
                 <div className="flex items-center">
                   <Mail className="mr-2" />
                   <Label htmlFor="event_name" className="text-left">
                     Email Composition:
                   </Label>
                 </div>
-                <Input type="body" placeholder="Body" />
+
+                <Textarea className="resize-none" placeholder="Enter email body here" />
               </div>
             </div>
+
             <DialogFooter className="sm:justify-end">
               <DialogClose asChild>
                 <Button type="button" variant="secondary">
