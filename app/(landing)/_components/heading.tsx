1afbb6c3bc9143ae4658106eb094e3a7bb827d0b
Hydemary <hydemaryalzbayne@gmail.com>
2024-01-23 21:36:01 +0800

    Update heading.tsx

--- app/(landing)/_components/heading.tsx
+++ app/(landing)/_components/heading.tsx
@@ -13,7 +13,7 @@ export const Heading = () => {
   return (
     <div className="max-w-3xl space-y-4">
       <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
-        Todin TodinTodinTodinTodinTodinTodinTodinTodinTodin.{" "}
+        Where Tasks align and Schedules shine.{" "}
         <span className="underline">Ergo</span>
       </h1>
       <h4 className="text-base sm:text-xl md:text-2xl font-medium">
