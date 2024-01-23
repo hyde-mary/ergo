138d5bcf3c86ed73d0a6e375b6f336c1ad99e7a1
Ospr3yyy <tristanrodrigo2103@gmail.com>
2024-01-23 21:29:48 +0800

    lorem ipsum to todintodintodin

--- app/(landing)/_components/heading.tsx
+++ app/(landing)/_components/heading.tsx
@@ -13,7 +13,7 @@ export const Heading = () => {
   return (
     <div className="max-w-3xl space-y-4">
       <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit.{" "}
+        Todin TodinTodinTodinTodinTodinTodinTodinTodinTodin.{" "}
         <span className="underline">Ergo</span>
       </h1>
       <h4 className="text-base sm:text-xl md:text-2xl font-medium">
